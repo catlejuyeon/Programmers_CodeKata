7d847b9a406d8861b396e5a8148554642ee54040
castlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2025-11-07 09:26:58 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum =0; 
+        
+        for (int i = 0; i < absolutes.length; i++) {
+            if(signs[i]){
+                sum=sum+absolutes[i];
+            }else {
+                sum=sum-absolutes[i];
+            }
+        }
+        return sum; 
+    }
+}
